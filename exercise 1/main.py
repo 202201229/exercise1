928488ffe6e20c41ba4febcb6b27235282598641
202201229 <143926869+202201229@users.noreply.github.com>
2023-09-03 17:08:45 +0800

    Add files via upload

--- exercise 1/main.py
+++ exercise 1/main.py
@@ -16,17 +16,16 @@ def replace_word(filename, old_word, new_word):
 word_to_count = "terrible"
 file_to_read = "file_to_read.txt"
 
-# 统计单词出现次数
+# Count the occurrences of a word
 total_occurrences = count_occurrences(word_to_count, file_to_read)
-print(f"单词 '{word_to_count}' 出现的总次数: {total_occurrences}")
+print(f"Total occurrences of the word '{word_to_count}': {total_occurrences}")
 
-# 替换单词并写入 result.txt
+# Replace the word and write to result.txt
 if total_occurrences > 0:
     if total_occurrences % 2 == 0:
         replace_word(file_to_read, word_to_count, "pathetic")
     else:
         replace_word(file_to_read, word_to_count, "marvellous")
-    print("替换完成。请查看 result.txt 获取更新后的内容。")
+    print("Replacement completed. Please check result.txt for the updated content.")
 else:
-    print("未找到该单词的出现次数。")
-
+    print("No occurrences of the word found.")
